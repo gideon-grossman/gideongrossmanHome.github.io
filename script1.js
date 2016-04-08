3a07aec517e691ec407fd401602a87fb34f4df93
gideongrossmanHome <gideongrossman@gmail.com>
2016-04-08 01:41:52 -1000

    add no note possibility

--- script1.js
+++ script1.js
@@ -15,17 +15,24 @@ var snareNote = document.getElementsByClassName("snareNote")[0];
 
 var showSheetMusic = function() {
   staff.style.visibility = 'visible';
-  snareNote.style.visibility = 'visible';
+
   var drumToHit = whichDrum();
-  if (drumToHit === "baseDrum"){
+  if (drumToHit === "none"){
+    snareNote.style.visibility = 'hidden';
+  }
+  else {
+      snareNote.style.visibility = 'visible';
+    if (drumToHit === "snareDrum") {
+  snareNote.style.bottom = "72px";
+  }
+  else if (drumToHit === "baseDrum"){
     snareNote.style.bottom = "23px";
   }
-  else if (drumToHit === "snareDrum"){
-    snareNote.style.bottom = "70px";
   }
 };
 
 title.addEventListener('click', showSheetMusic);
+
 //Drum kit image grow
 var drumPic = document.getElementById("drumkitImage");
 console.log("drumPic = " + drumPic.alt);
@@ -34,29 +41,33 @@ var startTime = new Date().getTime();
 console.log("startTime = "+startTime);
 var grow = function () {
   var currTime = new Date().getTime();
-  var newWidth = 10 + ((currTime - startTime) / 1000) * 100;
+  var newWidth = 10 + ((currTime - startTime) / 1000) * 30;
   console.log("drumPic.width = " + drumPic.width);
   if (drumPic.width < 200){
   drumPic.style.width = newWidth + "px";
-    console.log("drumPic.style.width = " + drumPic.style.width);
-  window.requestAnimationFrame(grow);
+ console.log("drumPic.style.width = " + drumPic.style.width);
+ window.requestAnimationFrame(grow);
   }
 };
 grow();
 
-var trueOrFalse = function(){
-  var answer = Math.round(Math.random());
-  console.log(answer); return answer;
+//generates a random integer
+var randomIntGenerator = function(){
+  var answer = Math.floor(Math.random()*3);
+  console.log("random integer = "+answer);
+  return answer;
 };
 
+//chooses which drum or no drum to hit
 var whichDrum = function() {
-  var trueOrFalseOutput = trueOrFalse();
-  if (trueOrFalseOutput === 0) {
+  var randomInt = randomIntGenerator();
+  if (randomInt === 0) {
     return "baseDrum";
   }
-  else if (trueOrFalseOutput === 1) {
+  else if (randomInt === 1) {
     return "snareDrum";
   }
+  else if (randomInt === 2) {
+    return "none";
+  }
 };
-
-console.log(whichDrum());
